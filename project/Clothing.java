a00d351914cbb7661d1a06b98b26f925e09be50b
kgryciuk <kgryciuk03@gmail.com>
2025-12-15 16:56:01 -0500

    Update Clothing.java

--- project/Clothing.java
+++ project/Clothing.java
@@ -4,10 +4,11 @@ public class Clothing extends Product {
 	public String clothingType;//is it a t-shirt, pants, sweater, etc
 	public String color;//what is the color: red, green, blue, etc
 	
-	public Clothing(String n,double p,int num,String ct,String c) {
+	public Clothing(String n,double p,int num,String ct,String c) {//puts name, price and number of items into the superclass and adds the type and color
 		super(n,p,num);
 		clothingType=ct;
 		color=c;
 	}
 	
+
 }
