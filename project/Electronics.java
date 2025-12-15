17c2ae3c2ed191b3370a4143bb7fc3d508ebc888
kgryciuk <kgryciuk03@gmail.com>
2025-12-15 16:56:47 -0500

    Update Electronics.java

--- project/Electronics.java
+++ project/Electronics.java
@@ -5,11 +5,12 @@ public class Electronics extends Product {//inherits from product class
 	public String brand;//who made it apple samsung, google, etc
 	public int year;//year device was released ex 2024
 	
-	public Electronics(String n,double p,int num,String t,String b,int y) {
+	public Electronics(String n,double p,int num,String t,String b,int y) {//puts name, price and number of items into the superclass and adds the devices type, the brand and the year its made
 		super(n,p,num);
 		deviceType=t;
 		brand=b;
 		year=y;
 	}
 	
+
 }
